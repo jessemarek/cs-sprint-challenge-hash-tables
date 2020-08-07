4247c965337b2183597826162ce8d19d304f23c9
unknown <dethgrenade@gmail.com>
2020-08-07 08:54:10 -0700

    WiP ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,30 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # dict to store total weight and which indices
+    d = {}
 
-    return None
+    # store the index numbers for answer
+    indices = []
+
+    # loop through list and add each weight to the list
+    # along with the index number
+    for i in range(length):
+        if weights[i] not in d:
+            d[weights[i]] = i
+
+    # check each weight. if (limit - weight) is in d we have
+    # another weight that adds up to the limit
+    for i in range(length):
+        if limit - weights[i] in d:
+            indices.append(i)
+
+    # if the list is not empty sort it in reverse order
+    # and return tuple of index nums
+    if len(indices) >= 1:
+        indices = sorted(indices, reverse=True)
+
+        return tuple(indices)
+
+    else:
+        return None
