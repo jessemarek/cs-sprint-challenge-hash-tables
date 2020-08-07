ba1d374346249dda7e185556762f9b448f3e1389
unknown <dethgrenade@gmail.com>
2020-08-07 09:28:23 -0700

    WiP ex2 done!

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,22 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # dict to store the tickets source and destination
+    d = {}
+
+    # list of the route order
+    route = []
+
+    # fill the dict with the source - destination key - value pairs
+    for i in range(length):
+        if tickets[i].source not in d:
+            d[tickets[i].source] = tickets[i].destination
+
+    # add the origin to the route list
+    route.append(d["NONE"])
+
+    # build the chain of tickets in order
+    for i in range(1, length):
+        route.append(d[route[i-1]])
 
     return route
